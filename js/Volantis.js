09e89232939a7ce23fb6b5e3a6f8e0fccea0943a
haisky <51472079+xiaobaisky@users.noreply.github.com>
2020-05-10 08:55:55 +0800

    Update Volantis.js

--- js/Volantis.js
+++ js/Volantis.js
@@ -1,52 +1,35 @@
-'use strict';
+/* eslint-disable */
+(function ($) {
+  "use strict";
+  function setTabs() {
+    const $tabs = $(".tabs");
+    if ($tabs.length === 0) return;
+    let $navs = $tabs.find(".nav-tabs .tab");
+    for (var i = 0; i < $navs.length; i++) {
+      let $a = $tabs.find($navs[i].children[0]);
+      $a.addClass($a.attr("href"));
+      $a.removeAttr("href");
+    }
+    $(".tabs .nav-tabs").on("click", "a", (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      let $tab = $(e.target.parentElement.parentElement.parentElement);
+      $tab.find(".nav-tabs .active").removeClass("active");
+      $tab.find(e.target.parentElement).addClass("active");
+      $tab.find(".tab-content .active").removeClass("active");
+      $tab.find($(e.target).attr("class")).addClass("active");
+      return false;
+    });
+  }
 
-function postCheckbox(args) {
-  args = args.join(' ').split(',')
-  var cls = ''
-  var text = ''
-  var checked = false
-  if (args.length > 1) {
-    cls = (args[0] || '').trim()
-    if (cls.length > 0) {
-      cls = ' ' + cls
-    }
-    if (cls.indexOf('checked') > -1) {
-      checked = true
-    }
-    text = (args[1] || '').trim()
-  } else if (args.length > 0) {
-    text = (args[0] || '').trim()
-  }
-  if (text.length > 0) {
-    return `<div class='checkbox${cls}'><input type="checkbox" ${ checked ? 'checked="checked"' : '' }/>
-            ${hexo.render.renderSync({text: text, engine: 'markdown'}).split('\n').join('')}
-            </div>`
-  }
-}
-function postRadio(args) {
-  args = args.join(' ').split(',')
-  var cls = ''
-  var text = ''
-  var checked = false
-  if (args.length > 1) {
-    cls = (args[0] || '').trim()
-    if (cls.length > 0) {
-      cls = ' ' + cls
-    }
-    if (cls.indexOf('checked') > -1) {
-      checked = true
-    }
-    text = (args[1] || '').trim()
-  } else if (args.length > 0) {
-    text = (args[0] || '').trim()
-  }
-  if (text.length > 0) {
-    return `<div class='checkbox${cls}'><input type="radio" ${ checked ? 'checked="checked"' : '' }/>
-            ${hexo.render.renderSync({text: text, engine: 'markdown'}).split('\n').join('')}
-            </div>`
-  }
-}
-// {% checkbox text %}
-// {% checkbox checked, text %}
-// {% checkbox color checked, text %}
-hexo.extend.tag.register('checkbox', postCheckbox);
+  $(function () {
+    setTabs();
+    // $(".article .video-container").fitVids();
+    $(".scroll-down").on("click", function () {
+      scrolltoElement(".l_body");
+    });
+    setTimeout(function () {
+      $("#loading-bar-wrapper").fadeOut(500);
+    }, 300);
+  });
+})(jQuery);
